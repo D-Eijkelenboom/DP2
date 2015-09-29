8f1fe52554c51ef0dcb8ad4d30ace6b7fabc86c7
dgaeijke <eijkelenboomdaniel@hotmail.com>
2015-09-29 15:46:39 +0200

    les 5

--- compiler/compiler/JumpNode.cs
+++ compiler/compiler/JumpNode.cs
@@ -8,5 +8,11 @@ namespace compiler
 {
     class JumpNode : Node
     {
+        public Node JumpToNode { get; set; }
+        //public override Node Execute()
+        //{
+        //    Next.Execute();
+        //    return this;
+        //}
     }
 }
--- compiler/compiler/Node.cs
+++ compiler/compiler/Node.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Compiler;
 
 namespace compiler
 {
     class Node
     {
+        public Node()
+        {
+            Next = null;
+        }
+
         public Node(Node next)
         {
             Next = next;
         }
+
         public Node Next { get; set; }
+
         public virtual Node Execute()
         {
             return null;
